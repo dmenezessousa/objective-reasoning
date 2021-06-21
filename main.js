0c359cd24d8b066b1808d98e6d291fc0f06ee1bf
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-06-21 15:58:19 -0400

    homework

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+//isAdmin ========================================
+
+const { KeyObject } = require("crypto");
+
+function isAdmin(user){
+    if(user.userRole === 'ADMIN'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//getEmail =======================================
+
+function getEmail(user){
+    return user.firstName.toLowerCase() + '.' + user.lastName.toLowerCase() + '@codeimmersives.com';
+}
+
+//getPlaylistLength ==============================
+function getPlaylistLength (key){
+    return key.songs.length;
+}
+
+//getHardestHomework =============================
+function getHardestHomework (homework){
+    if(homework.length === 0){
+        return '';
+    }
+    homework.sort(function(a,b){
+        return a.averageScore - b.averageScore;
+    });
+    
+
+    return homework[0].name;
+}
+
+//createPhonebook ================================
+function createPhonebook(name, number){
+    let arr = {};
+    for(i = 0 ; i < name.length; i++){
+        arr[name[i]] = number[i].toString();
+    }
+    return arr;
+}
 
 
 
